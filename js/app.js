09d42c599e05ec2035b76c69686e6274662f419d
Luis García <luis_grdz@hotmail.com>
2020-05-27 22:16:27 -0500

    Video 44: primeros pasos con el DOM y Scripting

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 // Eliminar de Local Storage
-localStorage.clear();
+// localStorage.clear();
+
+let elemento;
+
+elemento = document;
+elemento = document.all;
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+elemento = document.URL;
+elemento = document.characterSet;
+elemento = document.forms;
+elemento = document.images;
+elemento = document.images[2].getAttribute('src');
+
+console.log(elemento);
