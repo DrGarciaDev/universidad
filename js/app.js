2cd230e16e424abd7eb9f2bdd99efad55e782e5a
Luis García <luis_grdz@hotmail.com>
2020-05-28 21:38:21 -0500

    Video 46: seleccionando un elemento javascript

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-// getelementById
-let elemento;
+// query selector
 
-elemento = document.getElementById('encabezado');
-encabezado.style.background = '#333';
-encabezado.style.color = '#fff';
-encabezado.style.padding = '20px';
-encabezado.textContent = 'Los mejores cursos';
-encabezado.innerText = 'Los mejores cursos';
-console.log(elemento);
+// const encabezado = document.querySelector('#encabezado');
+
+const encabezado = document.querySelector('#principal a:nth-child(3)');
+
+console.log(encabezado);
+
+
+// query selector solo selecciona la primer aparicion del selector proporcionado
---
+++ js/app_2.js
@@ -0,0 +1,10 @@
+// getelementById
+let elemento;
+
+elemento = document.getElementById('encabezado');
+encabezado.style.background = '#333';
+encabezado.style.color = '#fff';
+encabezado.style.padding = '20px';
+encabezado.textContent = 'Los mejores cursos';
+encabezado.innerText = 'Los mejores cursos';
+console.log(elemento);
